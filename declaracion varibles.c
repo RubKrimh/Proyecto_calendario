b20731b53f72f5c2171e619531f088c23b46c15a
422106685 <422106685@pcpuma.acatlan.unam.mx>
2021-11-17 07:37:49 -0600

    espero que funcione

--- declaracion varibles.c
+++ declaracion varibles.c
@@ -1,7 +1,7 @@
-#inclide<stdio.h>
+#include<stdio.h>
 #include<time.h>
 
-{
+int main(){
 int i,j,k,m,l,cont;
 int dia,mes,anio,semana,moduloMes,febrero,total_mes;//Variables para almacenar el tiempo de la estructura tm.
 int seleccion=1;					//Variable para un ciclo de respuestas.
