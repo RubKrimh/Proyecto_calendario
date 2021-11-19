a51b7024ac2647d177e6819986d549ee0e6c0735
RubKrimh <408112859@pcpuma.acatlan.unam.mx>
2021-11-19 16:33:30 -0600

    Update declaracion varibles.c

--- declaracion varibles.c
+++ declaracion varibles.c
@@ -90,7 +90,49 @@ int main()
 				case 11:printf("\t\tDICIEMBRE\t%d",anio);
 				break;
 				}
-	 
+	 if (febrero==29)  
+			{
+			switch (mes)
+				{
+					case 0:
+					moduloMes=0;
+					break;
+					case 1:
+					moduloMes=3;
+					break;
+					case 2:
+					moduloMes=4;
+					break;
+					case 3:
+					moduloMes=0;
+					break;
+					case 4:
+					moduloMes=2;
+					break;
+					case 5:
+					moduloMes=5;
+					break;
+					case 6:
+					moduloMes=0;
+					break;
+					case 7:
+					moduloMes=3;
+					break;
+					case 8:
+					moduloMes=6;
+					break;
+					case 9:
+					moduloMes=1;
+					break;
+					case 10:
+					moduloMes=4;
+					break;
+					case 11:
+					moduloMes=6;
+					break;
+				}
+
+			}
 				
 		semana=((anio-1)%7+ ((anio-1)/4 -(3*((anio-1)/100+1)/4))%7+moduloMes+1%7)%7;
 		printf("\nDo\tLu\tMar\tMier\tJue\tVier\tSab\n");				
